b3d0c000e7700b34a1903ee661fd55cbcb6b1928
VirussInside <artiom.vallat@gmail.com>
2019-04-04 16:51:07 +0200

    Création des classes de base

---
+++ Audiotheque/IntelligentPlaylist.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Audiotheque
+{
+    class IntelligentPlaylist : Playlist
+    {
+        private List<Artist> artistList;
+        private int lowerRating;
+        private MusicLibrary library;
+
+
+        public IntelligentPlaylist(string name, MusicLibrary library): base(name) {
+
+            this.library = library;
+        }
+
+        public void addArtist(Artist artist) {
+
+        }
+
+        public void clearArtists() { }
+
+        public void setLowestRating(int lowestRating) {
+
+        }
+    }
+}
